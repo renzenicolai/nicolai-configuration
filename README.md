48361d313900e040c1581635ce53c993261362ad
Renze Nicolai <renze@rnplus.nl>
2021-06-18 00:24:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple configuration file parsing library for Node.js
+
+This library was originally part of my Spacecore open source project, this repository makes the library available under MIT license.
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+* @license
+* Copyright 2021 Renze Nicolai
+* This code is released under the MIT license.
+* SPDX-License-Identifier: MIT
+*/
+
+"use strict";
+
+const fs = require("fs");
+
+class Configuration {
+    constructor(filename) {
+        if (filename === null) {
+            this._data = {};
+        } else {
+            try {
+                this._data = JSON.parse(fs.readFileSync(filename));
+            } catch (error) {
+                throw Error("Unable to read configuration file", error);
+            }
+        }
+    }
+
+    get(/* ... */) {
+        for (let i = 0; i < arguments.length; i++) {
+            if (typeof arguments[i] != "string") {
+                throw Error("Configuration parameter get called with invalid arguments");
+            }
+        }
+        let result = this._data;
+        if (arguments.length > 0) {
+            try {
+                for (let i = 0; i < arguments.length; i++) {
+                    result = result[arguments[i]];
+                }
+            } catch (error) {
+                result = null;
+            }
+        }
+        if (typeof result === "undefined") {
+            result = null;
+        }
+        return result;
+    }
+}
+
+module.exports = Configuration;
